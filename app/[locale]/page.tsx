ce5f7e9fcaf78145407b5ef77fe2c24037389c45
Adam Buryšek <114564710+AdamBurysek@users.noreply.github.com>
2024-05-13 16:08:32 +0200

    feat - add projects

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,7 +1,16 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
+import AdamCodeV2 from '../../public/projects-images-home/adam-code-v2.png';
+import AdamPlanetOld from '../../public/projects-images-home/adam-planet-v1.png';
 import AdamPlanetImage from '../../public/projects-images-home/adam-planet.png';
+import GroceryBud from '../../public/projects-images-home/grocery-bud.png';
+import IdealCity from '../../public/projects-images-home/ideal-city.png';
+import MetalQuiz from '../../public/projects-images-home/metal-quiz.png';
+import Modrice from '../../public/projects-images-home/modrice.png';
+import SeaLevelRise from '../../public/projects-images-home/sea-level-rise.png';
+import TravelingSalesman from '../../public/projects-images-home/traveling-salesman.png';
+import VidaMenu from '../../public/projects-images-home/vida-menu.png';
 import MiniLogosBox from '../components/MiniLogos/MiniLogosBox';
 import TechnologyAnimation from '../components/TechnologyAnimation/TechnologyAnimation';
 import initTranslations from '../i18n';
@@ -48,15 +57,17 @@ const Home = async ({ params: { locale } }: LocaleParams) => {
       <section id="myprojects">
         <h2>{t('projects-header')}</h2>
         <div className={styles.projectsContainer} id="myprojects">
+          {/* PROJECT START */}
           <div>
             <Link href="/adamplanet">
               <div className={styles.projectHeader}>
-                <h3>Adam Planet</h3>
+                <h3>{t('project-adam-planet')}</h3>
                 <p className={styles.projectDate}>2024</p>
               </div>
               <Image
-                alt="Adam Planet Webpage"
+                alt={t('project-adam-planet')}
                 className={styles.projectImage}
+                // loading="eager"
                 placeholder="blur"
                 priority
                 src={AdamPlanetImage}
@@ -64,6 +75,173 @@ const Home = async ({ params: { locale } }: LocaleParams) => {
             </Link>
             <MiniLogosBox show={['Next', 'TypeScript', 'ThreeJs']} />
           </div>
+          {/* PROJECT END */}
+          {/* PROJECT START */}
+          <div>
+            <Link href="/adamplanet">
+              <div className={styles.projectHeader}>
+                <h3>{t('project-adam-code')}</h3>
+                <p className={styles.projectDate}>2023</p>
+              </div>
+              <Image
+                alt={t('project-adam-code')}
+                className={styles.projectImage}
+                loading="eager"
+                placeholder="blur"
+                src={AdamCodeV2}
+              />
+            </Link>
+            <MiniLogosBox
+              show={['React', 'TypeScript', 'Mongo', 'Cloudinary', 'Vite']}
+            />
+          </div>
+          {/* PROJECT END */}
+          {/* PROJECT START */}
+          <div>
+            <Link href="/adamplanet">
+              <div className={styles.projectHeader}>
+                <h3>{t('project-sea-level-rise')}</h3>
+                <p className={styles.projectDate}>2023</p>
+              </div>
+              <Image
+                alt={t('project-sea-level-rise')}
+                className={styles.projectImage}
+                loading="eager"
+                placeholder="blur"
+                src={SeaLevelRise}
+              />
+            </Link>
+            <MiniLogosBox show={['React', 'TypeScript', 'Electron', 'Vite']} />
+          </div>
+          {/* PROJECT END */}
+          {/* PROJECT START */}
+          <div>
+            <Link href="/adamplanet">
+              <div className={styles.projectHeader}>
+                <h3>{t('project-ideal-city')}</h3>
+                <p className={styles.projectDate}>2023</p>
+              </div>
+              <Image
+                alt={t('project-ideal-city')}
+                className={styles.projectImage}
+                loading="eager"
+                placeholder="blur"
+                src={IdealCity}
+              />
+            </Link>
+            <MiniLogosBox
+              show={['React', 'TypeScript', 'Electron', 'Vite', 'Mongo']}
+            />
+          </div>
+          {/* PROJECT END */}
+          {/* PROJECT START */}
+          <div>
+            <Link href="/adamplanet">
+              <div className={styles.projectHeader}>
+                <h3>{t('project-traveling-salesman')}</h3>
+                <p className={styles.projectDate}>2023</p>
+              </div>
+              <Image
+                alt={t('project-traveling-salesman')}
+                className={styles.projectImage}
+                loading="eager"
+                placeholder="blur"
+                src={TravelingSalesman}
+              />
+            </Link>
+            <MiniLogosBox show={['React', 'TypeScript', 'Electron', 'Vite']} />
+          </div>
+          {/* PROJECT END */}
+          {/* PROJECT START */}
+          <div>
+            <Link href="/adamplanet">
+              <div className={styles.projectHeader}>
+                <h3>{t('project-vida-menu')}</h3>
+                <p className={styles.projectDate}>2023</p>
+              </div>
+              <Image
+                alt={t('project-vida-menu')}
+                className={styles.projectImage}
+                loading="eager"
+                placeholder="blur"
+                src={VidaMenu}
+              />
+            </Link>
+            <MiniLogosBox show={['React', 'TypeScript', 'Electron', 'Vite']} />
+          </div>
+          {/* PROJECT END */}
+          {/* PROJECT START */}
+          <div>
+            <Link href="/adamplanet">
+              <div className={styles.projectHeader}>
+                <h3>{t('project-metal-quiz')}</h3>
+                <p className={styles.projectDate}>2023</p>
+              </div>
+              <Image
+                alt={t('project-metal-quiz')}
+                className={styles.projectImage}
+                loading="eager"
+                placeholder="blur"
+                src={MetalQuiz}
+              />
+            </Link>
+            <MiniLogosBox show={['React', 'JavaScript']} />
+          </div>
+          {/* PROJECT END */}
+          {/* PROJECT START */}
+          <div>
+            <Link href="/adamplanet">
+              <div className={styles.projectHeader}>
+                <h3>{t('project-modrice')}</h3>
+                <p className={styles.projectDate}>2023</p>
+              </div>
+              <Image
+                alt={t('project-modrice')}
+                className={styles.projectImage}
+                loading="eager"
+                placeholder="blur"
+                src={Modrice}
+              />
+            </Link>
+            <MiniLogosBox show={['Html', 'Css', 'JavaScript']} />
+          </div>
+          {/* PROJECT END */}
+          {/* PROJECT START */}
+          <div>
+            <Link href="/adamplanet">
+              <div className={styles.projectHeader}>
+                <h3>{t('project-grocery')}</h3>
+                <p className={styles.projectDate}>2022</p>
+              </div>
+              <Image
+                alt={t('project-grocery')}
+                className={styles.projectImage}
+                loading="eager"
+                placeholder="blur"
+                src={GroceryBud}
+              />
+            </Link>
+            <MiniLogosBox show={['Html', 'Css', 'JavaScript']} />
+          </div>
+          {/* PROJECT END */}
+          {/* PROJECT START */}
+          <div>
+            <Link href="/projects/adam-planet-v1">
+              <div className={styles.projectHeader}>
+                <h3>{t('project-adam-planet-old')}</h3>
+                <p className={styles.projectDate}>2022</p>
+              </div>
+              <Image
+                alt={t('project-adam-planet-old')}
+                className={styles.projectImage}
+                loading="eager"
+                placeholder="blur"
+                src={AdamPlanetOld}
+              />
+            </Link>
+            <MiniLogosBox show={['Html', 'Css', 'JavaScript']} />
+          </div>
+          {/* PROJECT END */}
         </div>
       </section>
     </>
---
+++ app/[locale]/projects/adam-planet-v1/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const AdamPlanetOld = () => (
+  <>
+    <h1 className="text-gradient">Old Adam Planet</h1>
+    <video
+      autoPlay
+      loop
+      muted
+      src="/project-videos/adam-planet-old-video.mp4"
+    />
+  </>
+);
+
+export default AdamPlanetOld;
