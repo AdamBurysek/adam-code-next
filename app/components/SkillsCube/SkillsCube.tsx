67454757596660267fc1ae25715de0109e30c2d4
Adam Buryšek <114564710+AdamBurysek@users.noreply.github.com>
2024-05-28 11:37:38 +0200

    fix - webgl error

--- app/components/SkillsCube/SkillsCube.tsx
+++ app/components/SkillsCube/SkillsCube.tsx
@@ -3,62 +3,15 @@
 'use client';
 
 import { OrbitControls } from '@react-three/drei';
-import { Canvas, useFrame } from '@react-three/fiber';
-import React, { useEffect, useRef, useState } from 'react';
+import { Canvas, useFrame, useLoader } from '@react-three/fiber';
+import React, { useRef } from 'react';
+import { TextureLoader } from 'three';
 import type { Mesh } from 'three';
-import { CanvasTexture } from 'three';
 
 import styles from './SkillsCube.module.css';
 
 const Cube = () => {
   const mesh = useRef<Mesh>(null);
-  const [textures, setTextures] = useState<CanvasTexture[]>([]);
-
-  useEffect(() => {
-    let isMounted = true;
-
-    const loadTextures = async () => {
-      const svgs = [
-        '/cube-images/react-logo.svg',
-        '/cube-images/node-logo.svg',
-        '/cube-images/typescript-logo.svg',
-        '/cube-images/next-logo.svg',
-        '/cube-images/mongo-logo.svg',
-        '/cube-images/python-logo.svg',
-      ];
-
-      const newTextures = await Promise.all(
-        svgs.map(async (svgUrl) => {
-          const img = new Image();
-          img.src = svgUrl;
-          await img.decode();
-
-          const canvas = document.createElement('canvas');
-          canvas.width = img.width + 200;
-          canvas.height = img.height + 200;
-          const ctx = canvas.getContext('2d');
-
-          ctx!.fillStyle = 'white';
-          ctx!.fillRect(0, 0, canvas.width, canvas.height);
-
-          ctx!.drawImage(img, 100, 100);
-
-          return new CanvasTexture(canvas);
-        }),
-      );
-
-      if (isMounted) {
-        setTextures(newTextures);
-      }
-    };
-
-    // eslint-disable-next-line @typescript-eslint/no-floating-promises
-    loadTextures();
-
-    return () => {
-      isMounted = false;
-    };
-  }, []);
 
   useFrame((state, delta) => {
     if (mesh.current) {
@@ -68,34 +21,35 @@ const Cube = () => {
     }
   });
 
-  if (textures.length === 0) {
-    return null; // or a loading spinner
-  }
+  const texture1 = useLoader(TextureLoader, '/cube-images/react-logo.svg');
+  const texture2 = useLoader(TextureLoader, '/cube-images/node-logo.svg');
+  const texture3 = useLoader(TextureLoader, '/cube-images/typescript-logo.svg');
+  const texture4 = useLoader(TextureLoader, '/cube-images/next-logo.svg');
+  const texture5 = useLoader(TextureLoader, '/cube-images/mongo-logo.svg');
+  const texture6 = useLoader(TextureLoader, '/cube-images/python-logo.svg');
 
   return (
     <mesh ref={mesh}>
-      <boxGeometry args={[2.7, 2.7, 2.7]} />
-      {textures.map((texture, index) => (
-        <meshStandardMaterial
-          // eslint-disable-next-line react/no-array-index-key
-          key={index}
-          attach={`material-${index}`}
-          map={texture}
-        />
-      ))}
+      <boxGeometry args={[2.5, 2.5, 2.5]} />
+      <meshStandardMaterial attach="material-0" map={texture1} />
+      <meshStandardMaterial attach="material-1" map={texture2} />
+      <meshStandardMaterial attach="material-2" map={texture3} />
+      <meshStandardMaterial attach="material-3" map={texture4} />
+      <meshStandardMaterial attach="material-4" map={texture5} />
+      <meshStandardMaterial attach="material-5" map={texture6} />
     </mesh>
   );
 };
 
-const SkillsCube = () => (
+const CubeComponent = () => (
   <div className={styles.container}>
     <Canvas>
       <OrbitControls enablePan={false} enableZoom={false} />
-      <ambientLight intensity={0.5} />
-      <directionalLight intensity={2} position={[1, 1, 1]} />
+      <ambientLight intensity={2} />
+      <directionalLight position={[2, 1, 1]} />
       <Cube />
     </Canvas>
   </div>
 );
 
-export default SkillsCube;
+export default CubeComponent;
