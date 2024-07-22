aafa4a5ed2a9af46268267999ee2e9a549fda264
Adam Buryšek <114564710+AdamBurysek@users.noreply.github.com>
2024-07-22 22:28:48 +0200

    Add missing types

--- app/components/Navbar/navLinks.ts
+++ app/components/Navbar/navLinks.ts
@@ -1,4 +1,9 @@
-export const navLinks = [
+type NavLinks = {
+  name: string;
+  target: string;
+};
+
+export const navLinks: NavLinks[] = [
   { name: 'projects', target: '/#myprojects' },
   { name: 'about', target: 'about' },
   { name: 'skills', target: 'skills' },
