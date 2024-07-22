88e0290da364fbcf314dfce6f0f1d1f15236f876
Adam Buryšek <114564710+AdamBurysek@users.noreply.github.com>
2024-07-22 22:37:37 +0200

    Remove unused eslint-disable

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable import/no-extraneous-dependencies */
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import { dir } from 'i18next';
